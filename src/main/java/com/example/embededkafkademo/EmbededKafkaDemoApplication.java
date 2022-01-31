700e7eaeb5f924672c78b83d66d7ea913c7b8dfb
Brett Strydom <brett.strydom@johnlewis.co.uk>
2022-01-31 14:50:11 +0200

    initial

---
+++ src/main/java/com/example/embededkafkademo/EmbededKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.embededkafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbededKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbededKafkaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/embededkafkademo/EmbededKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.embededkafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbededKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
